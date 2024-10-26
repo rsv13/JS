9f68750cc5918544fbd5992d70fed550e8bc4b10
rsv13 <28686684+rsv13@users.noreply.github.com>
2024-10-26 00:01:31 +0000

    Comparison of Datatypes

---
+++ 01-basics/04-comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);  //false
+// console.log(null == 0); //false
+// console.log(null >=0);  //true
+
+
+// console.log(undefined == 0);    //false
+// console.log(undefined > 0);     //false
+// console.log(undefined >= 0);    //false
+
+//avoid comparison between 2 different datatypes.
+//===
+
+console.log("2" === 2);
